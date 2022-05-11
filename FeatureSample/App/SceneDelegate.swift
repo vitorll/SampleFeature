6f5d8e2a69bfe7e931b730d62ac992d250f757d8
Vitor Lopes Leonardi <vitor.leonardi@qantas.com.au>
2022-05-11 14:34:02 +1000

    Refactor app delegate files and remove boilerplate code

---
+++ FeatureSample/App/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  FeatureSample
+//
+//  Created by Vitor on 11/5/2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
